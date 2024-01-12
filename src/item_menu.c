bad67c1f131c9f8012ad94c7cc87241df25fa5cf
Fervstheone <pedrohalabi.info@gmail.com>
2024-01-12 11:25:59 -0300

    100 TM support

--- src/item_menu.c
+++ src/item_menu.c
@@ -926,10 +926,19 @@ static void GetItemName(u8 *dest, u16 itemId)
             StringExpandPlaceholders(dest, gText_NumberItem_HM);
         }
         else
-        {
-            // Get TM number
-            ConvertIntToDecimalStringN(gStringVar1, itemId - ITEM_TM01 + 1, STR_CONV_MODE_LEADING_ZEROS, 2);
-            StringExpandPlaceholders(dest, gText_NumberItem_TMBerry);
+         {
+            if (itemId < ITEM_TM100)
+            {
+                // Get TM number
+                ConvertIntToDecimalStringN(gStringVar1, itemId - ITEM_TM01 + 1, STR_CONV_MODE_LEADING_ZEROS, 2);
+                StringExpandPlaceholders(dest, gText_NumberItem_TMBerry);
+            }
+            else
+            {
+                // Get TM number
+                ConvertIntToDecimalStringN(gStringVar1, itemId - ITEM_TM01 + 1, STR_CONV_MODE_LEADING_ZEROS, 3);
+                StringExpandPlaceholders(dest, gText_NumberItem_TMBerry);
+            }
         }
         break;
     case BERRIES_POCKET:
@@ -942,7 +951,6 @@ static void GetItemName(u8 *dest, u16 itemId)
         break;
     }
 }
-
 static void BagMenu_MoveCursorCallback(s32 itemIndex, bool8 onInit, struct ListMenu *list)
 {
     if (onInit != TRUE)
